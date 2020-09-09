6eecd15f3241a82735d1ff365fbf89dfc32d8654
Eli Huebner <elimhuebner@gmail.com>
2020-09-09 19:41:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(array)
     line_array = []
     index = 1
     array.each do |person|
-      line_array.push (index. person)
+      line_array.push ("#{index}. #{person}")
       index += 1
     puts "The line is currently: #{line_array.join}"
     end
