40532825c3aef13854bcb2bc1d89b50b2c480c91
Eli Huebner <elimhuebner@gmail.com>
2020-09-09 19:16:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    index = 1
+    print "The line is currently: "
+    until index == array.length - 1
+      print "#{index}. #{array[index - 1]}, "
+    end
+  end
+end
